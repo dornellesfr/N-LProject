da959545b80b1ea08f53c79763c997613f4f522a
@diogenesdornelles <diogenes.dornelles@gmail.com>
2023-02-20 01:20:02 -0300

    nav-bar

---
+++ assets/navbarCollapse.js
@@ -0,0 +1,9 @@
+const $targetEl = document.getElementById('targetEl');
+const $triggerEl = document.getElementById('triggerEl');
+const options = {
+  onCollapse: () => {},
+  onExpand: () => {},
+  onToggle: () => {}
+}
+
+const collapse = new Collapse($targetEl, $triggerEl, options)
