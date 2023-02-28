cbdfc83a8aa52a680863a80aca36abc8260c661b
fernando r <fe.dornellesrocha@outlook.com>
2023-02-28 11:20:19 -0300

    style: change color navbar to blue

--- app/src/components/Navbar.jsx
+++ app/src/components/Navbar.jsx
@@ -31,10 +31,10 @@ function Navbar() {
         <div className="hidden w-full md:block md:w-auto" id="navbar-dropdown">
           <ul id="ul-navbar" className="flex flex-col items-center p-4 mt-4 rounded-lg bg-white md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white">
             <li>
-              <Link to="/" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 " aria-current="page">Home</Link>
+              <Link to="/" className="block py-2 pl-3 pr-4 hover:text-red-600 text-white bg-blue-700 rounded md:bg-transparent md:text-white md:p-0 " aria-current="page">Home</Link>
             </li>
             <li>
-              <button type="button" id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" className="flex items-center justify-between w-full py-2 pl-3 pr-4 font-medium text-white rounded hover:bg-blue-600 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto">
+              <button type="button" id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" className="flex items-center justify-between w-full py-2 pl-3 pr-4 font-medium text-white rounded hover:bg-blue-600 md:hover:bg-transparent md:border-0 md:hover:text-red-700 md:p-0 md:w-auto">
                 Mais
                 <svg className="w-5 h-5 ml-1" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" /></svg>
               </button>
@@ -54,19 +54,19 @@ function Navbar() {
               </div>
             </li>
             <li>
-              <Link to="/services" className="block py-2 pl-3 pr-4 text-white rounded hover:bg-blue-600 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Serviços</Link>
+              <Link to="/services" className="block py-2 pl-3 pr-4 text-white rounded hover:bg-blue-600 md:hover:bg-transparent md:border-0 md:hover:text-red-700 md:p-0">Serviços</Link>
             </li>
             <li>
-              <Link to="/about" className="block py-2 pl-3 pr-4 text-white rounded hover:bg-blue-600 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Sobre</Link>
+              <Link to="/about" className="block py-2 pl-3 pr-4 text-white rounded hover:bg-blue-600 md:hover:bg-transparent md:border-0 md:hover:text-red-700 md:p-0">Sobre</Link>
             </li>
             <li>
-              <Link to="/notfound" className="block py-2 pl-3 pr-4 text-white rounded hover:bg-blue-600 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Clientes</Link>
+              <Link to="/notfound" className="block py-2 pl-3 pr-4 text-white rounded hover:bg-blue-600 md:hover:bg-transparent md:border-0 md:hover:text-red-700 md:p-0">Clientes</Link>
             </li>
             <li>
-              <Link to="/contact" className="block py-2 pl-3 pr-4 text-white rounded hover:bg-blue-600 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Contato</Link>
+              <Link to="/contact" className="block py-2 pl-3 pr-4 text-white rounded hover:bg-blue-600 md:hover:bg-transparent md:border-0 md:hover:text-red-700 md:p-0">Contato</Link>
             </li>
             <li>
-              <button id="to-top-anchor" onClick={(event) => setToTopFunc(event)} className="m-auto w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center flex justify-center " type="button">
+              <button id="to-top-anchor" onClick={(event) => setToTopFunc(event)} className="m-auto w-full text-white bg-red-700 hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center flex justify-center " type="button">
                 Retornar
                 <svg className="w-4 h-4 ml-2 rotate-180" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7" /></svg>
               </button>
