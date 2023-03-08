07218ca3c03aae36d3e653387061cbdf8cce500d
fernando r <fe.dornellesrocha@outlook.com>
2023-03-08 20:48:51 -0300

    fix: adjusting images import

--- app/src/utils/variables.js
+++ app/src/utils/variables.js
@@ -1,55 +1,45 @@
 /* eslint-disable linebreak-style */
-import img73 from '../assets/images/servicos/73.jpg';
-import img84 from '../assets/images/servicos/84.jpg';
-import img53 from '../assets/images/servicos/53.jpg';
-import img54 from '../assets/images/servicos/54.jpg';
-import img11 from '../assets/images/servicos/11.jpg';
-import img13 from '../assets/images/servicos/13.jpg';
-import img30 from '../assets/images/servicos/30.jpg';
-import img1 from '../assets/images/servicos/1.jpg';
-import img4 from '../assets/images/servicos/4.jpg';
-import img6 from '../assets/images/servicos/6.jpg';
-import v1 from '../assets/videos/5.mp4';
+import assets from './images';
 
 export const CAROUSELS = [
   {
     id: 'carousel-1',
     items: [
       {
-        url: img73,
+        url: assets.img1,
         message: 'Trabalhamos na construção de estradas rurais',
       },
       {
-        url: img84,
+        url: assets.img1,
         message: 'Mais um de nossos serviços',
       },
       {
-        url: img53,
+        url: assets.img1,
         message: 'Outro serviço prestado na construção de estradas rurais',
       },
       {
-        url: img54,
+        url: assets.img1,
         message: 'A empresa possui diversos veículos em sua frota',
       },
       {
-        url: img11,
+        url: assets.img1,
         message: 'Buscamos sempre a satisfação do cliente',
       },
       {
-        url: img13,
+        url: assets.img1,
         message: 'Mais alguns veículos que compõem nossa frota',
       },
       {
-        url: img30,
+        url: assets.img1,
         message: 'Nosso objetivo é buscar o acesso a todas as localidades',
       },
-      { url: img1, message: '' },
+      { url: assets.img1, message: '' },
       {
-        url: img4,
+        url: assets.img1,
         message: 'Soluções adequadas às necessidades dos clientes',
       },
       {
-        url: img6,
+        url: assets.img1,
         message: 'Atuamos também na construção de pontes',
       },
     ],
@@ -59,15 +49,15 @@ export const CAROUSELS = [
   {
     id: 'carousel-2',
     items: [
-      { url: img54, message: '' },
-      { url: img54, message: '' },
-      { url: img54, message: '' },
-      { url: img54, message: '' },
-      { url: img54, message: '' },
-      { url: img54, message: '' },
-      { url: img54, message: '' },
-      { url: img54, message: '' },
-      { url: img54, message: '' },
+      { url: assets.img1, message: '' },
+      { url: assets.img1, message: '' },
+      { url: assets.img1, message: '' },
+      { url: assets.img1, message: '' },
+      { url: assets.img1, message: '' },
+      { url: assets.img1, message: '' },
+      { url: assets.img1, message: '' },
+      { url: assets.img1, message: '' },
+      { url: assets.img1, message: '' },
     ],
     number: 2,
     text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta similique eos, sed placeat vero debitis iste? Nostrum magnam id, commodi iusto, laborum reiciendis voluptas fugiat provident, ipsum earum sit tenetur?',
@@ -77,22 +67,12 @@ export const CAROUSELS = [
 export const VIDEOS = [
   {
     id: 'video-1',
-    item: v1,
+    item: assets.video1,
     number: 1,
   },
   {
     id: 'video-2',
-    item: './videos/1.mp4',
+    item: assets.video2,
     number: 2,
-  },
-  {
-    id: 'video-3',
-    item: './videos/3.mp4',
-    number: 3,
-  },
-  {
-    id: 'video-4',
-    item: './videos/4.mp4',
-    number: 4,
   },
 ];
