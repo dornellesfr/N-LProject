7f471e1cdc8863e0d8de2e6abea9bc96e796accb
fernando r <fe.dornellesrocha@outlook.com>
2023-03-08 22:24:38 -0300

    fix: change the button as href to to

--- app/src/pages/Services.jsx
+++ app/src/pages/Services.jsx
@@ -24,7 +24,7 @@ function Services() {
         <div id="videos" className="grid grid-cols-1">
           {VIDEOS.map((video, index) => <Video key={index} item={video.item} id={video.id} />)}
         </div>
-        <Link href="/sobre" id="btn-our-services" className="border-solid border-2 border-white text-white hover:backdrop-blur-sm focus:ring-4 focus:outline-none font-bold rounded-lg text-sm px-20 py-2.5 text-center hover:px-32 mt-10 self-center md:text-3xl md:hover:px-96">Conheça mais sobre a empresa</Link>
+        <Link to="/sobre" id="btn-our-services" className="border-solid border-2 border-white text-white hover:backdrop-blur-sm focus:ring-4 focus:outline-none font-bold rounded-lg text-sm px-20 py-2.5 text-center hover:px-32 mt-10 self-center md:text-3xl md:hover:px-96">Conheça mais sobre a empresa</Link>
       </main>
     </>
   );
