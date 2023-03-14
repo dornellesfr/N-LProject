85f906c1cf2cf5938fd6ca6daae2d605b19f0cf5
fernando r <fe.dornellesrocha@outlook.com>
2023-03-14 01:24:45 -0300

    chore:

--- app/src/pages/Contacts.jsx
+++ app/src/pages/Contacts.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import '../assets/css/styleContact.css';
 import Form from '../components/Form';
+import Whatsapp from '../components/Whatsapp';
+import Maps from '../components/Maps';
 
 function Contacts() {
   return (
-    <Form />
+    <>
+      <Form />
+      <Whatsapp className="page-contact-session-whatsapp" />
+      <Maps />
+    </>
   );
 }
 
