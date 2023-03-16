96cd42424eac28608fbc4b5422ec59652055f34f
diogenesdornelles <diogenes.dornelles@gamil.com>
2023-03-16 17:13:57 -0300

    whatsapp fix

--- app/src/pages/Contacts.jsx
+++ app/src/pages/Contacts.jsx
@@ -2,16 +2,35 @@ import React from 'react';
 import '../assets/css/styleContact.css';
 import Form from '../components/Form';
 import Whatsapp from '../components/Whatsapp';
-import Maps from '../components/Maps';
+import BtnWhatsapp from '../components/BtnWhatsapp';
+// import Maps from '../components/Maps';
 
-function Contacts() {
-  return (
-    <>
-      <Form />
-      <Whatsapp className="page-contact-session-whatsapp" />
-      <Maps />
-    </>
-  );
+class Contacts extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      display: 'none',
+    };
+    this.toggleWhatsapp = this.toggleWhatsapp.bind(this);
+  }
+
+  toggleWhatsapp() {
+    this.setState((prevState) => ({
+      display: prevState.display === 'none' ? 'grid' : 'none',
+    }));
+  }
+
+  render() {
+    const { display } = this.state;
+    return (
+      <>
+        <Form />
+        <Whatsapp display={display} />
+        {/* <Maps /> */}
+        <BtnWhatsapp onBtnClick={this.toggleWhatsapp} />
+      </>
+    );
+  }
 }
 
 export default Contacts;
