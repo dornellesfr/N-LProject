15db698498c05ab725eb1a5ddd9e5d80f82c2f44
fernando r <fe.dornellesrocha@outlook.com>
2023-03-16 22:06:38 -0300

    chore: adjusts

--- app/jest.config.js
+++ app/jest.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   // setupFilesAfterEnv: ['/home/fernando/Documentos/projetos-pessoais/NLSite/app/jest.setup.js'],
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  setupFilesAfterEnv: ['src/setupTest.js'],
 };
